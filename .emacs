8518fe7de464b845b834f38d13a7d4f1814747d3
Denise H. G <darcsis@gmail.com>
2010-10-23 16:39:46 +0800

    remove unnecessary lines in '.emacs'; typesetting mi-send-pr.el

--- .emacs
+++ .emacs
@@ -8,7 +8,7 @@
 
 ;; test wether it is under xwindow
 (if (string-match "x" (prin1-to-string window-system))
-	(setq mi-use-xwindow 1)
+    (setq mi-use-xwindow 1)
   (setq mi-use-xwindow nil))
 
 ;; default configuration direcotry
@@ -23,10 +23,10 @@
   (setq mi-startup-el-files (cdr mi-startup-el-files))
   (setq startup-el-file-bytecoded (concat startup-el-file "c"))
   (setq startup-elc-file (concat (concat mi-default-conf-dir
-										 (file-name-nondirectory startup-el-file)) "c"))
+					 (file-name-nondirectory startup-el-file)) "c"))
   (when (file-newer-than-file-p startup-el-file startup-elc-file)
-	(byte-compile-file startup-el-file)
-	(rename-file startup-el-file-bytecoded startup-elc-file t)))
+    (byte-compile-file startup-el-file)
+    (rename-file startup-el-file-bytecoded startup-elc-file t)))
 
 ;; at last, suck in the configurations.
 (mapc 'load-file mi-startup-elc-files)
@@ -34,16 +34,4 @@
 ;; first time startup completed
 (setq mi-startup-first-time nil)
 
-;; always last
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(canlock-password "6e00d6507e4005f137a3dadceeb7e5c9a63d0380"))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- )
+;; end of dot.emacs
--- mi-send-pr.el
+++ mi-send-pr.el
@@ -19,39 +19,48 @@
   "Major mode for very simple problem reports highlighting.")
 
 (defvar mi-pr-version "MiSendPr 0.3")
-(defvar mi-pr-tmp-directory "mi-send-pr" "Default send-pr tmp directory.")
-(defvar mi-pr-template "~/emacs/template/pr.template" "PR Template.")
 (defvar mi-pr-working-file nil "Default working file for sending pr.")
 (defvar mi-pr-working-buffer nil "Default working buffer for sending pr.")
 (defvar mi-pr-fill-list '() "A list for lines where empty fields shoule be filled.")
 (defvar mi-pr-reserve-word "TO BE FILLED" "Reserve word for problem reports.")
-(defvar mi-pr-subject-string "Problem report" "Fail safe subject for problem report.")
 (defvar mi-pr-attachment-filename nil "Attachment file name.")
 (defvar mi-pr-attachment-prettyname nil "Better file name handling.")
 (defvar mi-pr-filename-suffix ".pr" "Default filename suffix for a problem report.")
 (defvar mi-pr-key-input nil "Capture key input sequence while filling out a problem report.")
+(defcustom mi-pr-tmp-directory "/var/tmp"
+  "Default send-pr tmp directory. e.g. '/var/tmp'"
+  :type 'string
+  :group 'mi-send-pr)
+(defcustom mi-pr-template-path "~/emacs/template/pr.template"
+  "Absolute file path to PR Template. e.g. '~/emacs/template/pr.template'"
+  :type 'string
+  :group 'mi-send-pr)
+(defcustom mi-pr-subject-string "Problem report"
+  "Fail safe subject for problem report."
+  :type 'string
+  :group 'mi-send-pr)
 (defcustom mi-pr-default-to-address "freebsd-gnats-submit@freebsd.org"
   "Default gnats server address."
   :type 'string
-  :group 'data)
+  :group 'mi-send-pr)
 (defcustom mi-pr-organization-name "XBSD Networks"
   "Default organization name for mi-send-pr."
   :type 'string
-  :group 'data)
+  :group 'mi-send-pr)
 
 (defun mi-pr-fill-out-pr ()
   "Fill out a problem report."
   (interactive)
   (if (not (null mi-pr-fill-list))
-	  (progn
-		(goto-char (point-min))
-		(forward-line (- (pop mi-pr-fill-list) 1))
-		(end-of-line)
-		(setq mi-pr-key-input
-			  (read-key-sequence nil))
-		(while (not (equal " " mi-pr-key-input))
-		  (setq mi-pr-key-input (read-key-sequence nil)))
-		(delete-char (- 0 (+ 2 (string-width mi-pr-reserve-word)))))))
+      (progn
+	(goto-char (point-min))
+	(forward-line (- (pop mi-pr-fill-list) 1))
+	(end-of-line)
+	(setq mi-pr-key-input
+	      (read-key-sequence nil))
+	(while (not (equal " " mi-pr-key-input))
+	  (setq mi-pr-key-input (read-key-sequence nil)))
+	(delete-char (- 0 (+ 2 (string-width mi-pr-reserve-word)))))))
 
 (defun mi-pr-post-fill ()
   "Post fill any unfilled items."
@@ -59,8 +68,8 @@
   (goto-char (point-min))
   (while (search-forward-regexp "^>Synopsis:[ ]+.*$" (point-max) t))
   (if (not (null (match-string 0)))
-	  (setq mi-pr-subject-string
-			(replace-regexp-in-string "^>Synopsis:[ ]+[\t]+" "" (match-string 0))))
+      (setq mi-pr-subject-string
+	    (replace-regexp-in-string "^>Synopsis:[ ]+[\t]+" "" (match-string 0))))
   (goto-char (point-min))
   (insert (concat "Subject: " mi-pr-subject-string "\n")))
 
@@ -69,21 +78,21 @@
   (interactive)
   (save-buffer)
   (if (yes-or-no-p "Really send the problem report? ")
-	  (progn
-		(mi-message-smtp-send)
-		(smtpmail-send-it)
-		(bury-buffer mi-pr-working-buffer)
-		(kill-buffer mi-pr-working-buffer))
-	(message "User quitted sending!")))
+      (progn
+	(mi-message-smtp-send)
+	(smtpmail-send-it)
+	(bury-buffer mi-pr-working-buffer)
+	(kill-buffer mi-pr-working-buffer))
+    (message "Message sending cancelled!")))
 
 (defun mi-pr-build-fill-list ()
   "Build a fill list."
   (interactive)
   (progn
-	(goto-char (point-min))
-	(while (re-search-forward (concat "\\[" mi-pr-reserve-word "\\]") nil t)
-	  (add-to-list 'mi-pr-fill-list (line-number-at-pos nil)))
-	(setq mi-pr-fill-list (nreverse mi-pr-fill-list))))
+    (goto-char (point-min))
+    (while (re-search-forward (concat "\\[" mi-pr-reserve-word "\\]") nil t)
+      (add-to-list 'mi-pr-fill-list (line-number-at-pos nil)))
+    (setq mi-pr-fill-list (nreverse mi-pr-fill-list))))
 
 (defun mi-pr-attach-file ()
   "Attach a file to a problem report."
@@ -91,16 +100,16 @@
   (goto-char (point-max))
   (setq mi-pr-attachment-filename (read-file-name "File to attach: "))
   (if (< 0 (string-width mi-pr-attachment-filename))
-	  (progn
-		(setq
-		 mi-pr-attachment-prettyname (replace-regexp-in-string ".*\\/" "" mi-pr-attachment-filename)
-		 mi-pr-attachment-prettyname (replace-regexp-in-string "^\\." "dot." mi-pr-attachment-prettyname))
-		; (newline)
-		(insert (concat "--- " mi-pr-attachment-prettyname " begins here ---\n"))
-		(insert-file-contents mi-pr-attachment-filename)
-		(goto-char (point-max))
-		(insert (concat "--- " mi-pr-attachment-prettyname " ends here ---")))
-	(message "OK, no attachments.")))
+      (progn
+	(setq
+	 mi-pr-attachment-prettyname (replace-regexp-in-string ".*\\/" "" mi-pr-attachment-filename)
+	 mi-pr-attachment-prettyname (replace-regexp-in-string "^\\." "dot." mi-pr-attachment-prettyname))
+	;; (newline)
+	(insert (concat "--- " mi-pr-attachment-prettyname " begins here ---\n"))
+	(insert-file-contents mi-pr-attachment-filename)
+	(goto-char (point-max))
+	(insert (concat "--- " mi-pr-attachment-prettyname " ends here ---")))
+    (message "OK, no attachments.")))
 
 (defun mi-pr-prepare-pr ()
   "Prepare a problem report with needed info."
@@ -108,53 +117,53 @@
   (set-buffer mi-pr-working-buffer)
   (goto-char (point-min))
   (while (re-search-forward "[<]+.*[>]+" nil t)
-	(replace-match (concat "\t[" mi-pr-reserve-word "]") t))
+    (replace-match (concat "\t[" mi-pr-reserve-word "]") t))
   (goto-char (point-min))
   (while (re-search-forward "^MI-SEND-PR>To:.*$" nil t)
-	(replace-match (concat "To: " mi-pr-default-to-address) t))
+    (replace-match (concat "To: " mi-pr-default-to-address) t))
   (goto-char (point-min))
   (while (re-search-forward "^MI-SEND-PR>From:.*$" nil t)
-	(replace-match (concat "From: " user-full-name " <" user-mail-address ">") t))
+    (replace-match (concat "From: " user-full-name " <" user-mail-address ">") t))
   (goto-char (point-min))
   (while (re-search-forward "^MI-SEND-PR>Reply-To:.*$" nil t)
-	(replace-match (concat "Reply-To: " user-full-name " <" user-mail-address ">") t))
+    (replace-match (concat "Reply-To: " user-full-name " <" user-mail-address ">") t))
   (goto-char (point-min))
   (while (re-search-forward "^MI-SEND-PR>X-send-pr-version:.*$" nil t)
-	(replace-match (concat "X-send-pr-version: " mi-pr-version) t))
+    (replace-match (concat "X-send-pr-version: " mi-pr-version) t))
   (goto-char (point-min))
   (while (re-search-forward "^MI-SEND-PR>>Submitter-Id:.*$" nil t)
-	(replace-match ">Submitter-Id: \tcurrent-users" t))
+    (replace-match ">Submitter-Id: \tcurrent-users" t))
   (goto-char (point-min))
   (while (re-search-forward "^MI-SEND-PR>>Originator:.*$" nil t)
-	(replace-match (concat ">Originator: \t" user-full-name) t))
+    (replace-match (concat ">Originator: \t" user-full-name) t))
   (goto-char (point-min))
   (while (re-search-forward "^MI-SEND-PR>>Organization:.*$" nil t)
-	(replace-match (concat ">Organization: \t" mi-pr-organization-name) t))
+    (replace-match (concat ">Organization: \t" mi-pr-organization-name) t))
   (goto-char (point-min))
   (while (re-search-forward "^MI-SEND-PR>>Confidential:.*$" nil t)
-	(replace-match ">Confidential: \tno" t))
+    (replace-match ">Confidential: \tno" t))
   (goto-char (point-min))
   (while (re-search-forward "^MI-SEND-PR>>Release:.*$" nil t)
-	(replace-match (concat ">Release: \t" (shell-command-to-string "echo -n `uname -smr`")) t))
+    (replace-match (concat ">Release: \t" (shell-command-to-string "echo -n `uname -smr`")) t))
   (goto-char (point-min))
   (while (re-search-forward "^MI-SEND-PR>System:.*$" nil t)
-	(replace-match (concat "System: " (shell-command-to-string "echo -n `uname -a`")) t))
+    (replace-match (concat "System: " (shell-command-to-string "echo -n `uname -a`")) t))
   (goto-char (point-min))
   (while (re-search-forward "^MI-SEND-PR>" nil t)
-	(replace-match "" t)))
- 
+    (replace-match "" t)))
+
 (defun mi-pr-make-pr ()
   "Make a problem report."
   (interactive)
-  (make-directory (concat "/var/tmp/" mi-pr-tmp-directory) "/var/tmp")
+  (make-directory (concat mi-pr-tmp-directory "/mi-send-pr") "/var/tmp")
   (setq mi-pr-working-file
-  		(make-temp-file
-		 (concat "/var/tmp/" mi-pr-tmp-directory "/" user-login-name "-")
-		 nil
-		 mi-pr-filename-suffix))
+	(make-temp-file
+	 (concat mi-pr-tmp-directory "/mi-send-pr/" user-login-name "-")
+	 nil
+	 mi-pr-filename-suffix))
   (setq mi-pr-working-buffer (create-file-buffer mi-pr-working-file))
   (set-buffer mi-pr-working-buffer)
-  (insert-file-contents-literally mi-pr-template)
+  (insert-file-contents-literally mi-pr-template-path)
   (switch-to-buffer mi-pr-working-buffer)
   (write-file mi-pr-working-file)
   (local-set-key [tab] 'mi-pr-fill-out-pr)
