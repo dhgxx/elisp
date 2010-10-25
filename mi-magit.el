6308bc9ca1cc33086454c8e11aacc22e5a5a56da
Denise H. G <darcsis@gmail.com>
2010-10-26 02:08:01 +0800

    modified key binding "\C-cm" to magit stuff; but "\C-ct" to time stamp stuff.

--- mi-magit.el
+++ mi-magit.el
@@ -1,12 +1,11 @@
-;; mi-magit.el -- emacs interface to git.
+;;; mi-magit.el -- emacs interface to git.
 
 (require 'magit)
 
-(setq magit-repo-dirs '("~/repos/git/scripts"
-			"~/repos/git/www"
-			"~/repos/github/bsd"
-			"~/repos/github/elisp"
-			"~/repos/github/misc"))
+(setq magit-repo-dirs '("~/repos/git"
+			"~/repos/github"))
 
 (global-set-key "\C-cm" nil)
 (global-set-key "\C-cms" 'magit-status)
+
+;;; mi-magit.el ends here
