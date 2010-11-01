5102be6631a7ac3055291913bc0e2b12d2ec2581
Denise H. G <darcsis@gmail.com>
2010-11-02 04:59:44 +0800

    more tests on elisp code highlighting for HTML.

--- mi-highlight.el
+++ mi-highlight.el
@@ -1,6 +1,6 @@
 ;;; elisp code highlighting for HTML.
 
-(defvar mi-html-elisp-defs-list
+(defvar mi-html-elisp-defvars-list
   '("defvar" "defcustom")
   "Elisp defs list.")
 
@@ -9,16 +9,22 @@
   "Elisp defuns list.")
 
 (defvar mi-html-elisp-keywords-list
-  '("eval-after-load" "if" "lambda" "let" "progn" "save-excursion" "when" "while")
+  '("eval-after-load" "if" "lambda" "let" "progn" "save-excursion" "when" "while" "with-current-buffer")
   "Elisp keywords.")
 
 ;; a temporary list to hold search results.
 (defvar mi-html-elisp-tmp-list nil)
 
 ;; custom variables
-(defcustom mi-html-color-elisp-quoted
+(defcustom mi-html-color-elisp-single-quoted
+  "#4cc417"
+  "HTML color code for `single' quoted string in Elisp."
+  :type 'string
+  :group 'mi-highlight)
+
+(defcustom mi-html-color-elisp-double-quoted
   "#e66c2c"
-  "HTML color code for quoted string in Elisp."
+  "HTML color code for double quoted string in Elisp."
   :type 'string
   :group 'mi-highlight)
 
@@ -51,83 +57,114 @@
   "Make Elisp code listing for HTML."
   (interactive)
 
-  ;; the matching order here matters!
-  ;; match quoted strings
-  (goto-char (point-min))
-  (while (search-forward-regexp "\\(\\(\".+[ ]*\"\\)+\\)" (point-max) t)
-    (replace-match (concat "<span style=\"color:"
-			   mi-html-color-elisp-quoted
-			   "\">\\&</span>")))
+  (save-excursion
 
-  ;; match comments
-  (goto-char (point-min))
-  (while (search-forward-regexp "^[ ]*\\([;]+.*\\)" (point-max) t)
-    (replace-match (concat "<span style=\"color:"
-			   mi-html-color-elisp-comments
-			   "\">\\&</span>")))
+    ;; make a new buffer for highlighting
+    (let ((tmp-buffer (get-buffer-create (concat (buffer-name (current-buffer))
+						 "<HH>"))))
+      (copy-to-buffer tmp-buffer (point-min) (point-max))
+      (set-buffer tmp-buffer)
+      (make-frame '((width . 80) (height . 40)))
 
-  ;; match `def*' in elisp code
-  (setq tmp-html-elisp-defs-list mi-html-elisp-defs-list
-	current-keyword (car tmp-html-elisp-defs-list))
-  (while current-keyword
-    (goto-char (point-min))
-    (while (search-forward-regexp (concat "\\(" current-keyword "\\)[ ]"
-					  "\\(\\**\\(\\w?-?\\)+\\**\\)") (point-max) t)
-      (replace-match "<span style=\"color:#3bb9ff\">\\1</span>&nbsp;<span style=\"color:#eac117\">\\2</span>"))
-    (setq tmp-html-elisp-defs-list (cdr tmp-html-elisp-defs-list)
-	  current-keyword (car tmp-html-elisp-defs-list)))
+      ;; the matching order here matters!
 
-  ;; match `defun' in elisp code
-  (setq tmp-html-elisp-defuns-list mi-html-elisp-defuns-list
-	current-keyword (car tmp-html-elisp-defuns-list))
-  (while current-keyword
-    (goto-char (point-min))
-    (while (search-forward-regexp (concat "\\(" current-keyword "\\)[ ]"
-					  "\\(\\(\\w?[-]?\\)+\\)") (point-max) t)
-      (replace-match "<span style=\"color:#3bb9ff\">\\1</span>&nbsp;<span style=\"color:#2b60de\">\\2</span>"))
-    (setq tmp-html-elisp-defuns-list (cdr tmp-html-elisp-defuns-list)
-	  current-keyword (car tmp-html-elisp-defuns-list)))
+
+      ;; match double quoted strings
+      (progn
+	(goto-char (point-min))
+	(while (search-forward-regexp "\\([\"].*[\"]\\)" (point-max) t)
+	  (replace-match (concat "<span style=\"color:"
+				 mi-html-color-elisp-double-quoted
+				 "\">\\&</span>"))))
+
+      ;; match single quoted string
+      (progn
+	(goto-char (point-min))
+	(while (search-forward-regexp "[^\"][`]\\(.+\\)['][^\"]" (point-max) t)
+	  (replace-match (concat "&nbsp;`<span style=\"color:"
+				 mi-html-color-elisp-single-quoted
+				 "\">\\1</span>'&nbsp;"))))
+
+      ;; match comments
+      (progn
+	(goto-char (point-min))
+	(while (search-forward-regexp "\\([;]+[ ]*\\(\\w+[ ]*\\)+.*\\)" (point-max) t)
+	  (replace-match (concat "<span style=\"color:"
+				 mi-html-color-elisp-comments
+				 "\">\\&</span>"))))
+
+      ;; match `def*' in elisp code
+      (progn
+	(setq tmp-html-elisp-defvars-list mi-html-elisp-defvars-list
+	      current-keyword (car tmp-html-elisp-defvars-list))
+	(while current-keyword
+	  (goto-char (point-min))
+	  (while (search-forward-regexp (concat "\\(" current-keyword "\\)[ ]"
+						"\\(\\**\\(\\w?-?\\)+\\**\\)") (point-max) t)
+	    (replace-match "<span style=\"color:#3bb9ff\">\\1</span>&nbsp;<span style=\"color:#eac117\">\\2</span>"))
+	  (setq tmp-html-elisp-defvars-list (cdr tmp-html-elisp-defvars-list)
+		current-keyword (car tmp-html-elisp-defvars-list))))
+
+      ;; match `defun' in elisp code
+      (progn
+	(setq tmp-html-elisp-defuns-list mi-html-elisp-defuns-list
+	      current-keyword (car tmp-html-elisp-defuns-list))
+	(while current-keyword
+	  (goto-char (point-min))
+	  (while (search-forward-regexp (concat "\\(" current-keyword "\\)[ ]"
+						"\\(\\(\\w?[-]?\\)+\\)") (point-max) t)
+	    (replace-match "<span style=\"color:#3bb9ff\">\\1</span>&nbsp;<span style=\"color:#2b60de\">\\2</span>"))
+	  (setq tmp-html-elisp-defuns-list (cdr tmp-html-elisp-defuns-list)
+		current-keyword (car tmp-html-elisp-defuns-list))))
   
-  ;;  match elisp key words
-  (setq tmp-html-elisp-keywords-list mi-html-elisp-keywords-list
-	current-keyword (car tmp-html-elisp-keywords-list))
-  (while current-keyword
-    (goto-char (point-min))
-    (while (search-forward-regexp current-keyword  (point-max) t)
-      (replace-match "<span style=\"color:#3bb9ff\">\\&</span>"))
-    (setq tmp-html-elisp-keywords-list (cdr tmp-html-elisp-keywords-list)
-	  current-keyword (car tmp-html-elisp-keywords-list)))
+      ;;  match elisp key words
+      (progn
+	(setq tmp-html-elisp-keywords-list mi-html-elisp-keywords-list
+	      current-keyword (car tmp-html-elisp-keywords-list))
+	(while current-keyword
+	  (goto-char (point-min))
+	  (while (search-forward-regexp (concat "[(]\\("
+						current-keyword
+						"\\)[ ]*")
+					(point-max) t)
+	    (replace-match "<span style=\"color:#3bb9ff\">\\&</span>"))
+	  (setq tmp-html-elisp-keywords-list (cdr tmp-html-elisp-keywords-list)
+		current-keyword (car tmp-html-elisp-keywords-list))))
 
-  ;; match leading tab spaces
-  (progn
-    (goto-char (point-min))
-    (setq buffer-lines (line-number-at-pos (point-max))
-	  current-line (line-number-at-pos (point)))
-    (while (< current-line buffer-lines)
-      (move-to-column 0)
-      (while (looking-at "[\t]")
-	(replace-match "&nbsp;&nbsp;&nbsp;&nbsp;")
-	(forward-char))
-      (forward-line 1)
-      (setq current-line (+ 1 current-line))))
+      ;; match leading tab spaces
+      (progn
+	(goto-char (point-min))
+	(setq buffer-lines (line-number-at-pos (point-max))
+	      current-line (line-number-at-pos (point)))
+	(while (< current-line buffer-lines)
+	  (move-to-column 0)
+	  (while (looking-at "[\t]")
+	    (replace-match "&nbsp;&nbsp;&nbsp;&nbsp;")
+	    (forward-char))
+	  (forward-line 1)
+	  (setq current-line (+ 1 current-line))))
 
-  ;; match leading spaces
-  (progn
-    (goto-char (point-min))
-    (setq buffer-lines (line-number-at-pos (point-max))
-	  current-line (line-number-at-pos (point)))
-    (while (< current-line buffer-lines)
-      (move-to-column 0)
-      (while (looking-at "[ ]")
-	(replace-match "&nbsp;")
-	(forward-char))
-      (forward-line 1)
-      (setq current-line (+ 1 current-line))))
-
-  ;; match newline
-  (goto-char (+ 1 (point-min)))
-  (while (search-forward-regexp "\n" (point-max) t)
-    (replace-match "<br />\n")))
+      ;; match leading spaces
+      (progn
+	(goto-char (point-min))
+	(setq buffer-lines (line-number-at-pos (point-max))
+	      current-line (line-number-at-pos (point)))
+	(while (< current-line buffer-lines)
+	  (move-to-column 0)
+	  (while (looking-at "[ ]")
+	    (replace-match "&nbsp;")
+	    (forward-char))
+	  (forward-line 1)
+	  (setq current-line (+ 1 current-line))))
+    
+      ;; match newline
+      (progn
+	(goto-char (+ 1 (point-min)))
+	(while (search-forward-regexp "\n" (point-max) t)
+	  (replace-match "<br />\n")))
+      )
+    )
+  )
 
 (global-set-key "\C-ch" nil)
 (global-set-key "\C-che" 'mi-html-make-code-elisp)
