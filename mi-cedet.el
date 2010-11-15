5afd220db18bddc52d5c2f39765e63363d8de693
Denise H. G <tamgya@gmail.com>
2010-11-15 10:55:49 +0800

    removed typos.

--- mi-cedet.el
+++ mi-cedet.el
@@ -6,10 +6,11 @@
 
 ;; Enable EDE (Project Management) features
 (global-ede-mode 1)
- 
-(setq semantic-load-turn-useful-things-on t)                     *
+
+(setq semantic-load-turn-useful-things-on t)
 (semantic-load-enable-excessive-code-helpers)
 (semantic-load-enable-semantic-debugging-helpers)
  
 ;; Enable SRecode (Template management) minor-mode.
 (global-srecode-minor-mode 1)
+
